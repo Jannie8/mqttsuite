311fb984573c4ab6366275518233e9342d0eae15
Jannie8 <janniesaayman88@gmail.com>
2025-10-29 11:57:28 +0100

    details database

--- mqtt/lib/Mqtt.cpp
+++ mqtt/lib/Mqtt.cpp
@@ -240,8 +240,8 @@ namespace mqtt::mqtt::lib {
         , mariaDB({
               // Connection detail
               .hostname = "localhost",
-              .username = "root",
-              .password = "hagenberg",
+              .username = "remoteuser",
+              .password = "strongpassword",
               .database = "SensorDataDB",
               .port = 3306,
               .socket = "/run/mysqld/mysqld.sock",
