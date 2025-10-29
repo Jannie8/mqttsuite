cdab31066061297adc0799582c206bb7ac48ad2a
tamerlan <tamerlan.aslakhanov@gmail.com>
2025-10-29 13:01:30 +0100

    format commented out

--- mqtt/lib/Mqtt.cpp
+++ mqtt/lib/Mqtt.cpp
@@ -202,7 +202,7 @@ std::vector<std::string> static myformat(const std::string& prefix,
 
     return lines;
 }
-
+/*
 // 2025-05-28 17:46:11 0000000014358
 // Добавляем [[maybe_unused]]
 static const std::string formatAsLogString(const std::string& prefix, const std::string& headLine, const std::string& message) {
@@ -214,6 +214,7 @@ static const std::string formatAsLogString(const std::string& prefix, const std:
     formatStr.pop_back();
     return formatStr;
 }
+*/
 
 namespace mqtt::mqtt::lib {
 
